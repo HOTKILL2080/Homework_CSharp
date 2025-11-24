4f16851ee5ba633285767186604360dda5fcb91c
HOTKILL2080 <kila4940@gmail.com>
2025-11-24 19:35:27 +0300

    part2

--- ConsoleAppFunc1/ConsoleAppFunc1/Program.cs
+++ ConsoleAppFunc1/ConsoleAppFunc1/Program.cs
@@ -14,31 +14,46 @@ namespace ConsoleAppFunc1
             string[] nums_arr = nums.Split();
             return string.Join(" ", nums_arr.Reverse());
         }
-        static int sum_num(string nums)
+        static string sum_str(string str)
         {
-            int num = 0;
-            foreach (string i in nums.Split())
+            string sum = "";
+            foreach (string i in str.Split())
             {
-                num += int.Parse(i);
+                sum += i;
             }
-            return num;
+            return sum;
         }
-        static int factorial(int fac)
+        static int factorial(int fact)
         {
-            int factorial = 1;
-            if (fac == 1 || fac == 0)
+            if (fact == 1 || fact == 0)
             {
-                factorial = 1;
+                return 1;
             }
             else
             {
-                for (int i = 1; i <= fac; i++)
-                {
-                    Console.WriteLine(i);
-                    factorial += factorial * (i-1);
-                }
+                return fact * factorial(fact - 1);
             }
-            return factorial;
+        }
+        static int calculate(string[] exp)
+        {
+            int result = 0;
+            if (exp[1] == "+")
+            {
+                result = int.Parse(exp[0]) + int.Parse(exp[2]);
+            }
+            else if  (exp[1] == "-")
+            {
+                result = int.Parse(exp[0]) - int.Parse(exp[2]);
+            }
+            else if (exp[1] == "*")
+            {
+                result = int.Parse(exp[0]) * int.Parse(exp[2]);
+            }
+            else if (exp[1] == "/")
+            {
+                result = int.Parse(exp[0]) / int.Parse(exp[2]);
+            }
+            return result;
         }
         static void Main(string[] args)
         {
@@ -51,16 +66,37 @@ namespace ConsoleAppFunc1
             //    4 задание
             //    Калькулятор  который будет принимать 2 числа(короче обычный
 
+            Console.WriteLine("Введите: число1 число2 через пробел");
             string nums = Console.ReadLine();
             nums = swap_num(nums);
             Console.WriteLine(nums);
 
-            int num = sum_num(nums);
-            Console.WriteLine(num);
+            Console.WriteLine("Сумма:");
+            string str = sum_str(nums);
+            Console.WriteLine(str);
 
-            int fac = int.Parse(Console.ReadLine());
-            fac = factorial(fac);
-            Console.WriteLine(fac);
+            try
+            {
+                Console.WriteLine("Введите факториал:");
+                int fact = int.Parse(Console.ReadLine());
+                int fact_res = factorial(fact);
+                Console.WriteLine("Факториал " + fact + ": " + fact_res);
+                
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Не правильный ввод");
+            }
+            try
+            {
+                Console.WriteLine("Введите: число1 знак(+ - / *) число2 через пробел");
+                string[] exp = Console.ReadLine().Split();
+                Console.WriteLine("Сумма: " + calculate(exp));
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Не правильный ввод");
+            }
         }
     }
 }
